e04cb553054175dfec992c5488a5c588c1ed18ee
svasenkov <aanher@gmail.com>
2021-05-28 22:35:04 +0300

    I did homework with Faker

---
+++ src/test/java/tests/StudentRegistrationFormWithFakerTests.java
@@ -0,0 +1,66 @@
+package tests;
+
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class StudentRegistrationFormWithFakerTests extends TestBase {
+
+    Faker faker = new Faker();
+
+    String firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            email = faker.internet().emailAddress(),
+            gender = "Other",
+            mobile = faker.phoneNumber().cellPhone(),
+            monthOfBirth = "May",
+            yearOfBirth = "2004",
+            dayOfBirth = "27",
+            subject1 = "Chemistry",
+            hobby1 = "Sports",
+            picture = "img/1.png",
+            currentAddress = faker.address().fullAddress(),
+            state = "Uttar Pradesh",
+            city = "Merrut";
+
+    @Test
+    void successfulRegistrationTest() {
+        open("https://demoqa.com/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+        $("#firstName").val(firstName);
+        $("#lastName").val(lastName);
+        $("#userEmail").val(email);
+        $("#genterWrapper").$(byText(gender)).click();
+        $("#userNumber").val(mobile);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption(monthOfBirth);
+        $(".react-datepicker__year-select").selectOption(yearOfBirth);
+        $(String.format(".react-datepicker__day--0%s:not(.react-datepicker__day--outside-month)", dayOfBirth)).click();
+        $("#subjectsInput").val(subject1).pressEnter();
+        $("#hobbiesWrapper").$(byText(hobby1)).click();
+        $("#uploadPicture").uploadFromClasspath("img/" + picture);
+        $("#currentAddress").val(currentAddress);
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText(state)).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText(city)).click();
+        $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $x("//td[text()='Student Name']").parent().shouldHave(text(firstName + " " + lastName));
+        $x("//td[text()='Student Email']").parent().shouldHave(text(email));
+        $x("//td[text()='Gender']").parent().shouldHave(text(gender));
+        $x("//td[text()='Mobile']").parent().shouldHave(text(mobile));
+        $x("//td[text()='Date of Birth']").parent().shouldHave(text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth));
+        $x("//td[text()='Subjects']").parent().shouldHave(text(subject1));
+        $x("//td[text()='Hobbies']").parent().shouldHave(text(hobby1));
+        $x("//td[text()='Picture']").parent().shouldHave(text(picture));
+        $x("//td[text()='Address']").parent().shouldHave(text(currentAddress));
+        $x("//td[text()='State and City']").parent().shouldHave(text(state + " " + city));
+    }
+
+}
